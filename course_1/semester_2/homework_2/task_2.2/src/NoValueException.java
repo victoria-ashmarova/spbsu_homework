c9983e0604270722379dc99b553958a2b8cea372
Victoria Ashmarova <victoria.ashmarova@gmail.com>
2017-05-20 17:51:23 +0700

    deleting not actual files

--- course_1/semester_2/homework_2/task_2.2/src/NoValueException.java
+++
@@ -1,5 +0,0 @@
-/**
- * Is thrown when there is no searched value in list
- */
-public class NoValueException extends Exception {
-}
--- course_1/semester_2/homework_2/task_2.2/src/PlaceException.java
+++
@@ -1,5 +0,0 @@
-/**
- * Is thrown when index of place to search is not correct
- */
-public class PlaceException extends Exception {
-}
