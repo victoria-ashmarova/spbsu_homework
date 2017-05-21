22e597e6c377b0b2013756372d5a9ec26779220b
Victoria Ashmarova <victoria.ashmarova@gmail.com>
2017-05-21 18:59:36 +0700

    upload task_5.1

---
+++ course_1/semester_2/homework_5/task_5.1/src/sample/Calculator.java
@@ -0,0 +1,36 @@
+package sample;
+
+/**
+ * Class, contains method, which return result of operation wuth two values.
+ */
+public class Calculator {
+    /**
+     * gets result of operation.
+     * @param firstValue is first member of operation
+     * @param secondValue is second member of operation
+     * @param operation is operation to do
+     * @return result of operation
+     */
+    public static double calculate(double firstValue, double secondValue, char operation){
+        switch (operation){
+            case '+':{
+                return firstValue + secondValue;
+            }
+            case '-':{
+                return firstValue - secondValue;
+            }
+            case '*':{
+                return firstValue * secondValue;
+            }
+            case '/':{
+                if (secondValue == 0){
+                    throw new ArithmeticException();
+                }
+                return firstValue / secondValue;
+            }
+            default:{
+                return 0;
+            }
+        }
+    }
+}
---
+++ course_1/semester_2/homework_5/task_5.1/tests/CalculatorTest.java
@@ -0,0 +1,77 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.junit.Assert;
+import org.junit.Test;
+import sample.Main;
+
+public class CalculatorTest extends Application{
+    private static Stage primaryStage;
+    private static AssertionError assertion = null;
+
+    /** starts the JavaFX application and waits for assertion fails */
+    @Test
+    public void test() {
+        assertion = null;
+        CalculatorTest.launch(CalculatorTest.class);
+        if (assertion != null)
+            throw assertion;
+    }
+
+    /**
+     * finds node on scene
+     * @param selector is fx:id of the node
+     * @param <T> is type of the node
+     * @return node
+     */
+    public static <T> T findNode(String selector) {
+        return (T) primaryStage.getScene().lookup(selector);
+    }
+
+    /** performs some simple actions with controls and checks if results are correct.*/
+    public void calculationTest() {
+        Spinner<Double> firstSpinner = findNode("#firstSpinner");
+        Spinner<Double> secondSpinner = findNode("#secondSpinner");
+        ComboBox actionBox = findNode("#actionBox");
+        TextField result = findNode("#resultField");
+
+        firstSpinner.getValueFactory().setValue(15.0);
+        secondSpinner.getValueFactory().setValue(3.0);
+
+        Assert.assertTrue(result.getText().equals("18.0"));
+
+        actionBox.getSelectionModel().select("-");
+        Assert.assertTrue(result.getText().equals("12.0"));
+
+        actionBox.getSelectionModel().select("*");
+        Assert.assertTrue(result.getText().equals("45.0"));
+
+        actionBox.getSelectionModel().select("/");
+        Assert.assertTrue(result.getText().equals("5.0"));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
+        Parent root = FXMLLoader.load(Main.class.getResource("Calculator.fxml"));
+        primaryStage.setTitle("Simple calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        try {
+            calculationTest();
+        } catch (AssertionError e) {
+            assertion = e;
+            primaryStage.close();
+        }
+
+        primaryStage.close();
+    }
+}
