90a77ffa5dfa915474d105e46e24831f0f088c03
Victoria Ashmarova <victoria.ashmarova@gmail.com>
2018-05-27 10:56:56 +0300

    changed describtions of conditions

--- course_2/semester_2/homework_3/task_3.4/task_3.4.hs
+++ course_2/semester_2/homework_3/task_3.4/task_3.4.hs
@@ -1,29 +1,24 @@
+pairsOfBrackets = [('(', ')'), ('[',']'), ('{','}')]
+
+isOpeningBracket :: Char -> Bool
+isOpeningBracket s = (filter (\c -> c == s) (map fst pairsOfBrackets)) /= []
+
+isClosingBracket :: Char -> Bool
+isClosingBracket s = (filter (\c -> c == s) (map snd pairsOfBrackets)) /= []
+
+arePair :: Char -> Char -> Bool
+arePair ob cb = (filter (\(a1,a2) -> a1 == ob && a2 == cb) pairsOfBrackets) /= []
+
 isCorrect :: [Char] -> Bool
-isCorrect line = isCorrect' line [] True where 
-	isOpenedBracket :: Char -> Bool
-	isOpenedBracket s = if (s == '(') || (s == '[') || (s == '{')
-						then True
-						else False
-	isClosedBracket :: Char -> Bool
-	isClosedBracket s = if (s == ')') || (s == ']') || (s == '}')
-						then True
-						else False
-	arePair :: Char -> Char -> Bool
-	arePair ob cb = if (ob == '('  && cb == ')') || (ob == '['  && cb == ']') || (ob == '{'  && cb == '}')
-					then True
-					else False
+isCorrect line = isCorrect' line [] True where 					
 	isCorrect' :: [Char] -> [Char] -> Bool -> Bool
 	isCorrect' [] [] result = result
 	isCorrect' [] bs result = False
-	isCorrect' (l:ls) [] result = if isOpenedBracket l
-								  then isCorrect' ls [l] result
-								  else if isClosedBracket l
-										then False
-										else isCorrect' ls [] result
-	isCorrect' (l:ls) (b:bs) result = if isOpenedBracket l
-										then isCorrect' ls (l:b:bs) result
-										else if isClosedBracket l
-											 then if arePair b l
-												  then isCorrect' ls bs result
-												  else False
-											 else isCorrect' ls (b:bs) result	
+	isCorrect' (l:ls) [] result | isOpeningBracket l = isCorrect' ls [l] result
+								| isClosingBracket l = False
+								| otherwise = isCorrect' ls [] result
+	isCorrect' (l:ls) (b:bs) result | isOpeningBracket l = isCorrect' ls (l:b:bs) result
+									| isClosingBracket l = if arePair b l
+														then isCorrect' ls bs result
+														else False
+								    |otherwise = isCorrect' ls (b:bs) result	
